609bccbe72eaecfff1f554e2fd9f3c84a82ad781
serhanni <33027656+serhanni@users.noreply.github.com>
2019-04-19 16:25:42 +0300

    rebrand to deviant

--- script/deviantd_setup.sh
+++ script/deviantd_setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Download latest node and install.
-bwklink=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
-mkdir -p /tmp/bulwark
-cd /tmp/bulwark
-curl -Lo bulwark.tar.gz $bwklink
-tar -xzf bulwark.tar.gz
+devlink=`curl -s https://api.github.com/repos/Deviantcoin/Source/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
+mkdir -p /tmp/deviant
+cd /tmp/deviant
+curl -Lo deviant.tar.gz $devlink
+tar -xzf deviant.tar.gz
 sudo mv ./bin/* /usr/local/bin
 cd
-rm -rf /tmp/bulwark
-mkdir ~/.bulwark
+rm -rf /tmp/deviant
+mkdir ~/.deviant
 
 # Setup configuration for node.
 rpcuser=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-cat >~/.bulwark/bulwark.conf <<EOL
+cat >~/.deviant/deviant.conf <<EOL
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 daemon=1
@@ -21,4 +21,4 @@ txindex=1
 EOL
 
 # Start node.
-bulwarkd
+deviantd
